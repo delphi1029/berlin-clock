0b669db9daab839df6c31a8728ceb53409443972
Deepak Lamba <lamba.deepak15@gmail.com>
2018-05-07 20:33:00 +0530

    Code refactored
    Code refactored

--- src/main/java/com/ubs/opsit/interviews/time/Hour.java
+++ src/main/java/com/ubs/opsit/interviews/time/Hour.java
@@ -2,33 +2,39 @@ package com.ubs.opsit.interviews.time;
 
 public class Hour {
 	
-	
 	private Integer hour;
+	private HourFirstRow firstrow;
+	private HourSecondRow secondrow;
 	
+	public Hour() {
+		super();
+		firstrow = new HourFirstRow();
+		secondrow = new HourSecondRow();
+	}
+
+	public Hour(HourFirstRow firstrow, HourSecondRow secondrow) {
+		super();
+		this.firstrow = firstrow;
+		this.secondrow = secondrow;
+	}
+
+
+
 	public void setHour(String hourStr) {
 		this.hour = Integer.parseInt(hourStr);
 	}
 	
+	
 	public StringBuilder display() {
-		StringBuilder hourDisplay = new StringBuilder();
-		HourFirstRow firstrow = new HourFirstRow();
-		HourSecondRow secondrow = new HourSecondRow();
-		
+		StringBuilder hourDisplay = new StringBuilder();	
 		hourDisplay.append(firstrow.display(hour));
 		hourDisplay.append(newLine());
 		hourDisplay.append(secondrow.display(hour));
 		hourDisplay.append(newLine());
 		return hourDisplay;
-		
 	}
 
 	private String newLine() {
 		return "\n";
 	}
-
-	
-
-	
-	
-
 }
