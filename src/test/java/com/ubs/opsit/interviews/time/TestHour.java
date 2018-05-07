94c29bf33bd9114a78fae4cf7e46f05e75bd2ab3
Deepak Lamba <lamba.deepak15@gmail.com>
2018-05-07 18:37:46 +0530

    Added Test
    Added Test

---
+++ src/test/java/com/ubs/opsit/interviews/time/TestHour.java
@@ -0,0 +1,49 @@
+package com.ubs.opsit.interviews.time;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestHour {
+	
+	Hour hour;
+	
+	@Before
+	public void init() {
+		hour = new Hour();
+	}
+	
+	
+	@Test
+	public void testDisplayMidnight() {
+		hour.setHour("00");
+		String expected = "OOOO\nOOOO\n";
+		
+		StringBuilder actual = hour.display();
+		
+		Assert.assertEquals(expected, actual.toString());
+	}
+	
+	@Test
+	public void testDisplayMidAfternoon() {
+		hour.setHour("13");
+		String expected = "RROO\nRRRO\n";
+		
+		StringBuilder actual = hour.display();
+		
+		Assert.assertEquals(expected, actual.toString());
+	}
+	
+	@Test
+	public void testDisplayJustBeforeMidnight() {
+		hour.setHour("23");
+		
+		String expected = "RRRR\nRRRO\n";
+		
+		StringBuilder actual = hour.display();
+		
+		Assert.assertEquals(expected, actual.toString());
+	}
+
+}
